bec0851ebf764817076d8410fa485cce4ba87b79
puuga <siwaoh@gmail.com>
2014-10-28 13:16:13 +0700

    init notice

--- main_menu.php
+++ main_menu.php
@@ -17,8 +17,37 @@
   <body>
     <?php include 'navbar.php'; ?>
 
-    <h1>
-      Main Menu
-    </h1>
+
+
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12">
+          <h1>
+            Notices
+          </h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12 bg-info">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <br/>
+
+      <div class="row">
+        <div class="col-md-12 bg-info">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+    </div>
+
   </body>
 </html>
