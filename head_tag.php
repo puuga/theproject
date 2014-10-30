324a7b0d669e3ca2a2150e0583b856591aa22de9
puuga <siwaoh@gmail.com>
2014-10-30 16:58:06 +0700

    new meta tag

--- head_tag.php
+++ head_tag.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
+    <meta name="author" content="Siwawes Wongcharoen, siwaoh@gmail.com">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
