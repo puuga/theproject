e0563401dfd0541d3b4f3ed4c5da77171249888f
puuga <siwaoh@gmail.com>
2014-11-13 14:00:17 +0700

    fixed link

--- class/String.php
+++ class/String.php
@@ -41,11 +41,11 @@
       const schedule_level_3 = "https://drive.google.com/open?id=0B9bzy3S3TMGRdnAzZXdsUVA2OVk&authuser=0";
       const schedule_level_4 = "https://drive.google.com/open?id=0B9bzy3S3TMGRdHZEbS01M2ZEWW8&authuser=0";
       const schedule_level_5 = "https://drive.google.com/open?id=0B9bzy3S3TMGRdWdKTWNZc0k0SlE&authuser=0";
-      const schedule_level_date_1 = "https://drive.google.com/open?id=0B9bzy3S3TMGRajhXemVvRmFCNlU&authuser=0";
-      const schedule_level_date_2 = "https://drive.google.com/open?id=0B9bzy3S3TMGRVU9OSkxsei1GM3c&authuser=0";
-      const schedule_level_date_3 = "https://drive.google.com/open?id=0B9bzy3S3TMGRT0I3MnZ3YkwxU1k&authuser=0";
-      const schedule_level_date_4 = "https://drive.google.com/open?id=0B9bzy3S3TMGRYlJILTVfX0pzQ1U&authuser=0";
-      const schedule_level_date_5 = "https://drive.google.com/open?id=0B9bzy3S3TMGRbk5kR3BNXy1COVE&authuser=0";
+      const schedule_level_date_1 = "https://drive.google.com/open?id=1ZDJyiLSClyLQz6BoAzZT4-Jac_5EmI6VzVBVaZb_31Q&authuser=0";
+      const schedule_level_date_2 = "https://drive.google.com/open?id=1TyI4Nd0yLFHILe67f3D8uoMSJOHGo1defPRmvhnAR7A&authuser=0";
+      const schedule_level_date_3 = "https://drive.google.com/open?id=1WAU_6g6lD5NxESZCpy5yDWXgewcxK-vGS92_keKRGgw&authuser=0";
+      const schedule_level_date_4 = "https://drive.google.com/open?id=1FAQQVQbradkS4L6h0kGjiKyOT50JAgN5-0zLKQSdWjY&authuser=0";
+      const schedule_level_date_5 = "https://drive.google.com/open?id=1Ajmi99s1EYxSa_0gUzIzX_MrntEK6zZAu95E-TsyJO0&authuser=0";
 
   }
 ?>
