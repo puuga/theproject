49f6ae033860a6c75e843de8482745cd3c31c55f
puuga <siwaoh@gmail.com>
2014-11-15 11:20:55 +0700

    fix ie6

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-  header('Location: main_menu.php');
+  $using_ie6 = (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.') !== FALSE);
+  if ($using_ie6) {
+    header('Location: not_support.php');
+  } else {
+    header('Location: main_menu.php');
+  }
+
 ?>
---
+++ not_support.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo String::booking; ?></title>
+  </head>
+  <body>
+    <center>
+      <p><h1>ขออภัย ระบบไม่สนับสนุน Browser ที่ท่านใช้งาน</h1></p>
+      <p>โปรดใช้ <a href="http://www.google.com/chrome/">Google Chrome</a> แทน</p>
+      <p></p>
+      <p></p>
+    </center>
+  </body>
+</html>
